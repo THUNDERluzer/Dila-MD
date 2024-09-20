1827621df606b7cbb8ed02137911183a37535923
THUNDERluzer <95018914+THUNDERluzer@users.noreply.github.com>
2024-09-20 10:56:47 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "4u8lyaBB#lIUvTWnMaKzs5nSBguL6DhavNsPNdnIglTlzz20EOOM",
+SESSION_ID: process.env.SESSION_ID || "1j9TyRYJ#yUXL94HIYcJc6ghBOZ1-iM3vHg7_3LDfZ_cCWe-c9Uc",
 MONGODB: process.env.MONGODB || "mongodb://mongo:AWtuAmGpatfmlwZoIaGuNsxLNsFfeLPV@autorack.proxy.rlwy.net:20819",
 };
